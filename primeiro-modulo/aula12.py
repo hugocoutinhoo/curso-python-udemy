3754291d3cb5e9e2a274aad19248122b8058a96e
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-22 17:01:30 -0300

    aulas9 - 12

---
+++ primeiro-modulo/aula12.py
@@ -0,0 +1,7 @@
+nome = 'Hugo'
+altura = 1.82
+peso = 94.4
+imc = peso / (altura * altura)
+
+print(nome, "tem ", altura, "de altura.")
+print("pesa", peso, "kg e seu IMC é de:", imc)
---
+++ primeiro-modulo/aula9.py
@@ -0,0 +1,21 @@
+# Operadores Aritméticos
+adicao = 10 + 10
+print('Adição', adicao)
+
+subtracao = 10 - 5
+print("Subtração", subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 # resultado (float)
+print('Divisão', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisão Inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('Exponenciação', exponenciacao)
+
+modulo = 25 % 2 # resto da divisão
+print('Módulo', modulo)
