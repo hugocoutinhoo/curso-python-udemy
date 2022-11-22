b8311b331b241734c40bcccb54f4151745f5597d
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-22 16:28:37 -0300

    aula8

---
+++ primeiro-modulo/aula8.py
@@ -0,0 +1,13 @@
+nome = 'Hugo'
+sobrenome = 'Coutinho'
+idade = 29
+ano_de_nasc = 2022 - idade
+maior_de_idade = idade >= 18
+altura = 1.82
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_de_nasc)
+print('Maior de idade?', maior_de_idade)
+print('Altura:', altura)
