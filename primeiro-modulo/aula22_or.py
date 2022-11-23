b6ab761985b5ac0490b8b70fccddb3d121e4aae9
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-23 12:14:44 -0300

    aulas22-24

--- primeiro-modulo/aula22_or.py
+++ primeiro-modulo/aula22_or.py
@@ -6,12 +6,19 @@
 # 0 0.0 '' False
 # Também existe o tipo None que é usado para representar um não valor.
 
-entrada = input('[E]ntrar [S]air: ')
-senha_digitada = input('Senha: ')
-senha_permitida = '123456'
+#entrada = input('[E]ntrar [S]air: ')
+#senha_digitada = input('Senha: ')
 
-if entrada == 'E' and senha_digitada == senha_permitida:
-    print('Entrar')
-else:
-    print('Sair')
+#senha_permitida = '123456'
+
+#if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
+#    print('Entrar')
+#else:
+#    print('Sair')
+
+#Avaliação de curto circuito
+senha = 0 or False or 0 or 'abc' or True
+print(senha)
+
+
 
