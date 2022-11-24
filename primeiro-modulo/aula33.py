9a55d6f0f0f8af5904a5c7dff3df8a08765bc75e
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2022-11-24 10:21:02 -0300

    aula33

--- primeiro-modulo/aula33.py
+++ primeiro-modulo/aula33.py
@@ -37,7 +37,15 @@ except:
 """
 faça um programa que peça o primeiro nome do usuário. Se o nome tiver 4 letras ou menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva "Seu nome é normal"; maior que 6 letras, escreva "Seu nome é muito grande".
 """
+nome = input('Digite seu nome: ')
+tamanho_nome = len(nome)
 
+if tamanho_nome <=4:
+    print('Seu nome é curto')
+elif tamanho_nome >=5 and tamanho_nome <= 6:
+    print('Seu nome é normal')
+else:
+    print('Seu nome é grande')
  
 
 
